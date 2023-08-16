2a7e14849538261a803a1eced6e39c8e0cab17b6
FernandaFonseca <fernanda_fonseca0107@hotmail.com>
2023-08-15 23:17:24 -0300

    Alteração pasta e execuçao

---
+++ QAACADEMY/qaacademy_online/src/Exercicios_easy/Execucao.java
@@ -0,0 +1,10 @@
+package Exercicios_easy;
+
+public class Execucao {
+    public static void main(String[] args) {
+        Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
+        String palavra = exercicio02_easy.retornarPalavra("Fernanda");
+        System.out.println(palavra); 
+    }
+    
+}
