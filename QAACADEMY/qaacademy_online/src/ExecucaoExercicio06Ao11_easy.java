b5107664f9d68c47791450bfcb664fd0eca550a4
FernandaFonseca <fernanda_fonseca0107@hotmail.com>
2023-08-19 22:38:39 -0300

    Refatoração Exercicio 07_easy

--- QAACADEMY/qaacademy_online/src/ExecucaoExercicio06Ao11_easy.java
+++ QAACADEMY/qaacademy_online/src/ExecucaoExercicio06Ao11_easy.java
@@ -1,6 +1,7 @@
 import javax.swing.JOptionPane;
 
 import Exercicios_easy.Exercicio06_easy;
+import Exercicios_easy.Exercicio07_easy;
 
 public class ExecucaoExercicio06Ao11_easy {
     public static void main(String[] args) {
@@ -16,6 +17,18 @@ public class ExecucaoExercicio06Ao11_easy {
 
         Exercicio06_easy exercicio06_easy = new Exercicio06_easy();
         System.out.println(exercicio06_easy.verificaMediaEAprovacao(nota1, nota2));
+
+        // Exercicio07_easy
+        String salarioDigitado;
+
+        salarioDigitado = JOptionPane.showInputDialog("Digite seu salário!");
+        double salario = Double.parseDouble(salarioDigitado);
+
+        Exercicio07_easy exercicio07_easy = new Exercicio07_easy();
+        System.out.println("O valor do INSS a ser pago é: R$ " + exercicio07_easy.calcularINSS(salario));
+
+        double inss = exercicio07_easy.calcularINSS(salario);
+        System.out.println("O valor do salário com desconto do INSS é: R$ " + (salario - inss));
 
     }
 
--- QAACADEMY/qaacademy_online/src/Exercicios_easy/Exercicio07_easy.java
+++ QAACADEMY/qaacademy_online/src/Exercicios_easy/Exercicio07_easy.java
@@ -1,38 +1,30 @@
 package Exercicios_easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio07_easy {
     
-    public static void main(String[] args) {
-
-        double salario, inss=0.0;
-        String salarioDigitado;
-
-        salarioDigitado = JOptionPane.showInputDialog("Digite seu salário!");
-        salario = Double.parseDouble(salarioDigitado); // atribuir o valor digitado para a variavel DOUBLE
-
+    public double calcularINSS(double salario) {
+        double inss=0.0;
+      
         if (salario <= 1045.00) {
-            inss = salario * 0.075;
+            return inss = salario * 0.075;
         }
 
         if (salario >= 1045.01 && salario <=2089.60) {
-            inss = salario * 0.09;
+            return inss = salario * 0.09;
         }
 
         if (salario >= 2089.61 && salario <=3134.40) {
-            inss = salario * 0.12;
+            return inss = salario * 0.12;
         }
 
         if (salario >= 3134.41 && salario <=6101.06) {
-            inss = salario * 0.14;
+            return inss = salario * 0.14;
         }
 
         if (salario >= 6101.07) {
-            inss = 6101.06 * 0.075;
+            return inss = 6101.06 * 0.075;
         }
-
-        System.out.println("O valor do INSS a ser pago é: R$ " + (inss));
-        System.out.println("O valor do salário com desconto do INSS é: R$ " + (salario-inss));
+        return 0.0;
+    
     }
 }
