8f6fa1335b2770bbcaf508a8886b37fc38681568
FernandaFonseca <fernanda_fonseca0107@hotmail.com>
2023-08-19 23:01:19 -0300

    Reformulação do Exercicio08_easy

--- QAACADEMY/qaacademy_online/src/ExecucaoExercicio06Ao11_easy.java
+++ QAACADEMY/qaacademy_online/src/ExecucaoExercicio06Ao11_easy.java
@@ -2,6 +2,7 @@ import javax.swing.JOptionPane;
 
 import Exercicios_easy.Exercicio06_easy;
 import Exercicios_easy.Exercicio07_easy;
+import Exercicios_easy.Exercicio08_easy;
 
 public class ExecucaoExercicio06Ao11_easy {
     public static void main(String[] args) {
@@ -21,7 +22,7 @@ public class ExecucaoExercicio06Ao11_easy {
         // Exercicio07_easy
         String salarioDigitado;
 
-        salarioDigitado = JOptionPane.showInputDialog("Digite seu salário!");
+        salarioDigitado = JOptionPane.showInputDialog("Digite seu salário para calcular o INSS!");
         double salario = Double.parseDouble(salarioDigitado);
 
         Exercicio07_easy exercicio07_easy = new Exercicio07_easy();
@@ -29,6 +30,19 @@ public class ExecucaoExercicio06Ao11_easy {
 
         double inss = exercicio07_easy.calcularINSS(salario);
         System.out.println("O valor do salário com desconto do INSS é: R$ " + (salario - inss));
+
+
+        // Exercicio08_easy
+        String salarioBrutoDigitado = JOptionPane.showInputDialog("Digite o valor do seu salário para calcular o Imposto de Renda!");
+        double salarioBruto = Double.parseDouble(salarioBrutoDigitado);
+
+        Exercicio08_easy exercicio08_easy = new Exercicio08_easy();
+        System.out.println("O seu salário bruto é = R$ " + (salarioBruto));
+
+        double impostoRenda = (salarioBruto - exercicio08_easy.calcularImpostoDeRenda(salarioBruto));
+        System.out.println("O Imposto de Renda a ser pago é = R$ " + (impostoRenda));
+
+        System.out.println("O salário líquido é = R$ " + exercicio08_easy.calcularImpostoDeRenda(salarioBruto));
 
     }
 
